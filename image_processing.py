188aaa243e85fbb9be0f783e14138e460285cb33
hamelmateo <hamel.mateo.fr@gmail.com>
2023-10-26 10:40:53 +0200

    Processing Working and Code cleaned

---
+++ image_processing.py
@@ -0,0 +1,131 @@
+"""
+Created on Thu Sep 19 10:35:22 2023
+
+@author: Mateo HAMEL
+"""
+
+try:
+    import cv2
+    import numpy as np
+    from typing import List, Tuple
+except ImportError as e:
+    raise ImportError(f"Required modules are missing. {e}")
+
+
+
+def compute_temporal_average(images: List[np.ndarray], window_size: int) -> List[np.ndarray]:
+    """
+    Compute the temporal average of a list of images over a specified window size.
+    
+    Args:
+        images (List[np.ndarray]): List of images to average.
+        window_size (int): Number of images to average over.
+
+    Returns:
+        List[np.ndarray]: List of temporally averaged images.
+    """
+    if not images:
+        raise ValueError("The list of images is empty.")
+    if not all(isinstance(image, np.ndarray) for image in images):
+        raise TypeError("All elements in the images list should be NumPy arrays.")
+    if window_size <= 0:
+        raise ValueError("Window size should be a positive integer.")
+    if len(images) < window_size:
+        raise ValueError(f"The number of images should be at least {window_size}")
+    
+    number_chunk = len(images) // window_size
+    array_imgs = np.array(images)
+
+    imgs_average = []
+
+    for i in range(number_chunk):
+        sum_img = np.zeros(images[0].shape, dtype=np.float16)
+        for j in range(window_size):
+            sum_img += array_imgs[i * window_size + j]
+        imgs_average.append(np.divide(sum_img, window_size).astype(np.uint8))
+
+    return imgs_average
+
+
+def apply_median_filter(images: List[np.ndarray], kernel_size: int) -> List[np.ndarray]:
+    """
+    Apply a median filter to a list of images.
+    
+    Args:
+        images (List[np.ndarray]): List of images to apply the median filter to.
+        kernel_size (int): Size of the kernel for the median filter.
+
+    Returns:
+        List[np.ndarray]: List of images after median filtering.
+    """
+    if not images:
+        raise ValueError("The list of images is empty.")
+    if not all(isinstance(image, np.ndarray) for image in images):
+        raise TypeError("All elements in the images list should be NumPy arrays.")
+    if kernel_size <= 0 or kernel_size % 2 == 0:
+        raise ValueError("Kernel size should be a positive odd integer.")
+    
+    filtered_imgs = [cv2.medianBlur(img, kernel_size) for img in images]
+    return filtered_imgs
+
+
+def apply_clahe(images: List[np.ndarray], clip_limit: float, tile_grid_size: Tuple[int, int]) -> List[np.ndarray]:
+    """
+    Apply Contrast Limited Adaptive Histogram Equalization (CLAHE) to a list of images.
+    
+    Args:
+        images (List[np.ndarray]): List of images to enhance.
+        clip_limit (float): Threshold for contrast limiting.
+        tile_grid_size (Tuple[int, int]): Size of the grid for histogram equalization.
+
+    Returns:
+        List[np.ndarray]: List of images after applying CLAHE.
+    """
+    if not images:
+        raise ValueError("The list of images is empty.")
+    if not all(isinstance(image, np.ndarray) for image in images):
+        raise TypeError("All elements in the images list should be NumPy arrays.")
+    if clip_limit <= 0:
+        raise ValueError("Clip limit should be a positive float.")
+    if len(tile_grid_size) != 2 or not all(isinstance(dim, int) and dim > 0 for dim in tile_grid_size):
+        raise ValueError("Tile grid size should be a tuple of two positive integers.")
+
+    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
+    proc_imgs = [clahe.apply(img) for img in images]
+    return proc_imgs
+
+
+def process_images(images: List[np.ndarray], temporal_average_window_size: int, median_kernel_size: int,
+                   clip_limit: float, tile_grid_size: Tuple[int, int]) -> List[np.ndarray]:
+    """
+    Process a list of images for further analysis.
+    
+    Args:
+        images (List[np.ndarray]): List of images to process.
+        temporal_average_window_size (int): Number of images over which to compute the temporal average.
+        median_kernel_size (int): Size of the kernel used for median filtering.
+        clip_limit (float): Threshold for contrast limiting in CLAHE.
+        tile_grid_size (Tuple[int, int]): Size of the grid for histogram equalization in CLAHE.
+
+    Returns:
+        List[np.ndarray]: List of processed images.
+    """
+    if not images:
+        raise ValueError("The list of images is empty.")
+    
+    # Remove noise
+    averaged_imgs = compute_temporal_average(images, temporal_average_window_size)
+    cv2.imshow("Averaged Image", cv2.resize(averaged_imgs[3], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
+
+    filtered_imgs = apply_median_filter(averaged_imgs, median_kernel_size)
+    cv2.imshow("Filtered Image", cv2.resize(filtered_imgs[3], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
+    
+    # Increase sharpness
+    # TODO
+
+    # Increase contrast
+    proc_imgs = apply_clahe(filtered_imgs, clip_limit, tile_grid_size)
+    cv2.imshow("CLAHE Image", cv2.resize(proc_imgs[3], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
+    cv2.waitKey(0)  # Wait indefinitely until a key is pressed
+    
+    return proc_imgs
--- main.py
+++ main.py
@@ -4,174 +4,77 @@ Created on Thu Sep 19 10:35:22 2023
 @author: Mateo HAMEL
 """
 
-# Standard library imports
+# Standard Library Imports
 import os
-import sys
-
-# Third-party library imports
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 import time
 
-# Global Variables
-RAW_IMAGES_PATH = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\raw_images'
-N_TEMP_AVERAGE = 10
-MEDIAN_KERNEL_SIZE = 5
+# Third-party Library Imports
+import cv2
+
+# Local Module Imports
+import image_processing
+
+
+# Configuration Constants
+RAW_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\raw_images'
+TEMPORAL_AVERAGE_WINDOW_SIZE = 10
+MEDIAN_FILTER_KERNEL_SIZE = 5
 CLAHE_CLIP_LIMIT = 2.0
 CLAHE_TILE_GRID_SIZE = (8, 8)
 
-def Open_images(path):
+
+def load_grayscale_images(directory_path):
     """
-    Open images from the folder raw_images and put them in grayscale for faster computing
+    Load images from the specified directory and convert them to grayscale.
 
-    Input:
-        path: path of folder raw_images
-    
-    Returns:
-        raw_imgs: List of images
-        filename: List of the name of the images 
-    """
-
-    filenames = [f for f in os.listdir(path) if f.endswith(('.png','.JPG','.jpeg'))]
-    print(f"Attempting to load {len(filenames)} images...")
-
-    raw_imgs = [cv2.imread(os.path.join(path, f), cv2.IMREAD_GRAYSCALE) for f in filenames]
-    
-    if not raw_imgs:
-        print("No images were loaded. Please check the file paths and formats.")
-
-    return raw_imgs, filenames
-
-
-def temporal_average(imgs_tmp, n):
-    """
-    Function that does a temporal average over N consecutive images
-    Warning: if the number of images is not a multiple of the kernel size, the last images will be lost.
-    
-    Inputs:
-        imgs_tmp: List of images to average
-        n: Number of images to average over
+    Args:
+        directory_path (str): The file path of the directory containing the images.
 
     Returns:
-        imgs_average: List of images averaged
+        tuple: A tuple containing two elements:
+            - A list of grayscale images.
+            - A list of filenames corresponding to the loaded images.
+
+    Raises:
+        FileNotFoundError: If the specified directory does not exist.
+        ValueError: If no images are found in the directory.
     """
-    #for img in imgs_tmp:
-    #    print(img.shape)
+    if not os.path.exists(directory_path):
+        raise FileNotFoundError(f"The specified directory does not exist: {directory_path}")
 
-    if len(imgs_tmp) < n:
-        raise ValueError(f"The number of images should be at least {n}")
-    
-    number_chunk = len(imgs_tmp) // n
-    array_imgs = np.array(imgs_tmp)
+    image_filenames = [filename for filename in os.listdir(directory_path) if filename.endswith(('.png', '.JPG', '.jpeg'))]
+    if not image_filenames:
+        raise ValueError("No images found in the specified directory.")
 
-    imgs_average = []
+    print(f"Attempting to load {len(image_filenames)} images...")
+    grayscale_images = [cv2.imread(os.path.join(directory_path, filename), cv2.IMREAD_GRAYSCALE) for filename in image_filenames]
 
-    for i in range(number_chunk):
-        sum_img = np.zeros(imgs_tmp[0].shape, dtype=np.float16)
-        for j in range(n):
-            sum_img += array_imgs[i*n+j]
-        imgs_average.append(np.divide(sum_img,n).astype(np.uint8))
+    if not grayscale_images:
+        raise ValueError("One or more images could not be loaded. Please check the file paths and formats.")
 
-    # Display averaged images
-    #for idx, img in enumerate(imgs_average):
-    #    cv2.imshow(f"Averaged Image {idx}", cv2.resize(img, None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    #    cv2.waitKey(0)  # Wait indefinitely until a key is pressed
+    return grayscale_images, image_filenames
 
-    return imgs_average
-
-def median_filtering(imgs_tmp, kernel_size):
-    """
-    Function that apply a median filter to images
-
-    Inputs:
-        imgs_tmp: List of images to apply filters to
-        kernel_size: Size of the median filter
-
-    Returns:
-        imgs_average: List of images filtered
-    """
-        
-    filtered_imgs = [cv2.medianBlur(img, kernel_size) for img in imgs_tmp]
-
-    # Display averaged images
-    #for idx, img in enumerate(filtered_imgs):
-    #    cv2.imshow(f"Averaged Image {idx}", cv2.resize(img, None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    #    cv2.waitKey(0)  # Wait indefinitely until a key is pressed
-
-    return filtered_imgs
-
-def clahe(imgs_tmp, clip_limit=CLAHE_CLIP_LIMIT, tile_grid_size=CLAHE_TILE_GRID_SIZE):
-    """
-    Function that applies Contrast Limited Adaptive Histogram Equalization (CLAHE) to the images.
-
-    Inputs:
-        imgs_tmp: List of images to enhance contrast
-        clip_limit: Threshold for contrast limiting
-        tile_grid_size: Size of grid for histogram equalization
-
-    Returns:
-        proc_imgs: List of images processed
-    """
-    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
-    proc_imgs = [clahe.apply(img) for img in imgs_tmp]
-
-    # Display processed images
-    #for idx, img in enumerate(proc_imgs):
-    #    cv2.imshow(f"CLAHE Image {idx}", cv2.resize(img, None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    #    cv2.waitKey(0)  # Wait indefinitely until a key is pressed
-
-    return proc_imgs
-
-
-def processing(imgs_tmp):
-    """
-    Process the images before NPs counting
-
-    Input:
-        imgs_tmp: List of images to process
-
-    Returns:
-        proc_imgs: List of images processed
-    """    
-    # Remove noise
-    averaged_imgs = temporal_average(imgs_tmp, N_TEMP_AVERAGE)
-    cv2.imshow(f"Averaged Image", cv2.resize(averaged_imgs[3], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    filtered_imgs = median_filtering(averaged_imgs, MEDIAN_KERNEL_SIZE)
-    cv2.imshow(f"Filtered Image", cv2.resize(filtered_imgs[3], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    
-    # Increase sharpness
-    #TODO
-
-    # Increase contrast
-    proc_imgs = clahe(filtered_imgs)
-    cv2.imshow(f"Clahe Image", cv2.resize(proc_imgs[3], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    cv2.waitKey(0)  # Wait indefinitely until a key is pressed
-    
-    return proc_imgs
 
 def main():
     """
-    Main function where the core logic of the script is placed.
+    Main execution function.
+
+    This function orchestrates the loading of images, their processing, and the subsequent display
+    of results. Execution time for each major step is printed to the console.
     """
-    
-    # Open raw images
-    start_time = time.time()  # Start the timer
+    try:
+        start_time = time.time()
+        raw_images, image_filenames = load_grayscale_images(RAW_IMAGES_DIRECTORY)
+        end_time = time.time()
+        print(f"Execution time for loading images: {end_time - start_time:.4f} seconds")
 
-    raw_imgs, filenames = Open_images(RAW_IMAGES_PATH)
-    #print(filenames)
-
-    end_time = time.time()  # End the timer
-    print(f"Execution time Open images: {end_time - start_time:.4f} seconds")
-
-    # Show raw images
-    #cv2.imshow("First Image", cv2.resize(raw_imgs[0], None, fx=0.20, fy=0.20, interpolation=cv2.INTER_AREA))
-    #cv2.waitKey(0)  # Wait indefinitely until a key is pressed
-
-    #Processing
-    proc_imgs = processing(raw_imgs)
-
-    pass
+        processed_images = image_processing.process_images(raw_images, TEMPORAL_AVERAGE_WINDOW_SIZE, MEDIAN_FILTER_KERNEL_SIZE, CLAHE_CLIP_LIMIT, CLAHE_TILE_GRID_SIZE)
+    except FileNotFoundError as fnf_err:
+        print(f"Error: {fnf_err}")
+    except ValueError as val_err:
+        print(f"Error: {val_err}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
-    main()  
+    main()
