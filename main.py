cef3766055e6010ce59d65842add4ab324d46fe3
hamelmateo <hamel.mateo.fr@gmail.com>
2023-11-05 12:22:54 +0100

    NPs area counting working but inaccurate

--- main.py
+++ main.py
@@ -12,18 +12,24 @@ import time
 import cv2
 import numpy as np
 from typing import List
+import pandas as pd
 
 # Local Module Imports
 import image_processing
 
 
 # Configuration Constants
-RAW_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\raw_images'
-PROCESSED_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\processed_images'
+RAW_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\01_images_raw'
+PROCESSED_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\02_images_processed'
+MASKED_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\03_images_masked'
+SEGMENTED_IMAGES_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\04_images_segmented'
+RESULTS_DIRECTORY = 'C:\\Users\\hamel\\OneDrive - Neosens Diagnostics\\04_Technology_Software\\Image Processing\\NEOSENS\\05_Results'
 TEMPORAL_AVERAGE_WINDOW_SIZE = 10
 MEDIAN_FILTER_KERNEL_SIZE = 5
 CLAHE_CLIP_LIMIT = 2.0
 CLAHE_TILE_GRID_SIZE = (8, 8)
+ADAPTIVE_THRESHOLDING_BLOCK_SIZE = 3 # Size of a pixel neighborhood that is used to calculate a threshold value.
+ADAPTIVE_THRESHOLDING_CONSTANT = 45 # Constant subtracted from the mean or weighted mean.
 
 
 def load_grayscale_images(directory_path):
@@ -57,8 +63,23 @@ def load_grayscale_images(directory_path):
 
     return grayscale_images, image_filenames
 
+def count_white_pixels(binary_image):
+    """
+    Count the number of white pixels in a binary image.
 
-def apply_nanoparticles_segmentation(images: List[np.ndarray], output_folder: str) -> List[np.ndarray]:
+    Args:
+        binary_image: Binary image where white pixels have the value 255.
+
+    Returns:
+        int: The number of white pixels.
+    """
+    # Assuming white pixels are 255, count all non-zero values
+    
+    return cv2.countNonZero(binary_image)
+
+
+
+def apply_nanoparticles_segmentation(images: List[np.ndarray], filenames: List[str], output_folder: str) -> List[np.ndarray]:
     """
     Apply Otsu thresholding followed by closing and Watershed segmentation.
 
@@ -72,55 +93,24 @@ def apply_nanoparticles_segmentation(images: List[np.ndarray], output_folder: st
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    seg_imgs = []
-    kernel = np.ones((3, 3), np.uint8)  # Kernel for morphological operations
+    segmented_images = []
+
 
     for i, img in enumerate(images):
         # Ensure the image is in grayscale
         gray = img if len(img.shape) == 2 else cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Save the original grayscale image
-        cv2.imwrite(os.path.join(output_folder, f"image_{i}_original.png"), gray)
+        # Apply adaptive thresholding
+        adaptive_thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                                cv2.THRESH_BINARY_INV, ADAPTIVE_THRESHOLDING_BLOCK_SIZE, ADAPTIVE_THRESHOLDING_CONSTANT)
 
-        # Apply Otsu's thresholding
-        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        cv2.imwrite(os.path.join(output_folder, f"image_{i}_thresholded.png"), thresh)
+        # Save the thresholded image
+        segmented_filename = f"segmented_{filenames[i]}"
+        cv2.imwrite(os.path.join(output_folder, segmented_filename), adaptive_thresh)
 
-        # Apply morphological closing to fill small holes and gaps
-        closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
-        cv2.imwrite(os.path.join(output_folder, f"image_{i}_closing.png"), closing)
+        segmented_images.append(adaptive_thresh)
 
-        # Remove small white regions
-        sure_bg = cv2.dilate(closing, kernel, iterations=3)
-
-        # Finding sure foreground area using distance transform and thresholding
-        dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 5)
-        _, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
-
-        # Finding unknown region
-        sure_fg = np.uint8(sure_fg)
-        unknown = cv2.subtract(sure_bg, sure_fg)
-
-        # Marker labelling
-        _, markers = cv2.connectedComponents(sure_fg)
-
-        # Add one to all labels so that sure background is not 0, but 1
-        markers = markers + 1
-
-        # Now, mark the region of unknown with zero
-        markers[unknown == 255] = 0
-
-        # Apply Watershed
-        img_color = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)  # Convert to BGR for watershed
-        cv2.watershed(img_color, markers)
-        img_color[markers == -1] = [0, 0, 255]  # Marking the boundaries with red color
-
-        # Save the final segmented image
-        cv2.imwrite(os.path.join(output_folder, f"image_{i}_segmented.png"), img_color)
-
-        seg_imgs.append(img_color)
-
-    return seg_imgs
+    return segmented_images
 
 def get_circle_roi(img: np.ndarray) -> np.ndarray:
     """
@@ -136,11 +126,11 @@ def get_circle_roi(img: np.ndarray) -> np.ndarray:
     
     def draw_circle(event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDBLCLK:
-            cv2.circle(mask, (x, y), param['radius'], (255, 255, 255), -1)
+            cv2.circle(mask, (x, y), param['radius'], 255, -1)
             param['finished'] = True
     
     cv2.namedWindow('image')
-    cv2.setMouseCallback('image', draw_circle, {'radius': 50, 'finished': False})
+    cv2.setMouseCallback('image', draw_circle, {'radius': 85, 'finished': False})
     
     while True:
         combined = cv2.addWeighted(img, 0.7, mask, 0.3, 0)
@@ -152,7 +142,8 @@ def get_circle_roi(img: np.ndarray) -> np.ndarray:
     
     return mask
 
-def apply_masking_roi(images: List[np.ndarray]) -> List[np.ndarray]:
+
+def apply_masking_roi(segm_images: List[np.ndarray], images: List[np.ndarray], filenames: List[str], output_folder: str) -> List[np.ndarray]:
     """
     Ask the user to define a circular ROI on one of the images and apply a masking on everything besides the ROI to all images of the list.
 
@@ -165,11 +156,20 @@ def apply_masking_roi(images: List[np.ndarray]) -> List[np.ndarray]:
     if not images:
         return []
     
-    # Use the first image to define the ROI
-    mask = get_circle_roi(images[0])
-    
-    # Apply the mask to all images
-    masked_imgs = [cv2.bitwise_and(img, img, mask=mask) for img in images]
+    masked_imgs = []
+    for i, img in enumerate(images):
+        # Use the image to define the ROI
+        mask = get_circle_roi(img)
+        
+        # Apply the mask to the image
+        masked_img = cv2.bitwise_and(segm_images[i], segm_images[i], mask=mask)
+        
+        # Save the final segmented image
+        masked_filename = f"masked_{filenames[i]}"
+        cv2.imwrite(os.path.join(output_folder, masked_filename), masked_img)
+
+        # Append the masked image and filename to the list
+        masked_imgs.append((masked_img, masked_filename))
     
     return masked_imgs
 
@@ -184,7 +184,7 @@ def main():
     try:
         # Load images
         start_time = time.time()
-        raw_images, _ = load_grayscale_images(RAW_IMAGES_DIRECTORY)
+        raw_images, filenames = load_grayscale_images(RAW_IMAGES_DIRECTORY)
         end_time = time.time()
         print(f"Execution time for loading images: {end_time - start_time:.4f} seconds")
 
@@ -192,9 +192,18 @@ def main():
         #processed_images = image_processing.process_images(raw_images, TEMPORAL_AVERAGE_WINDOW_SIZE, MEDIAN_FILTER_KERNEL_SIZE, CLAHE_CLIP_LIMIT, CLAHE_TILE_GRID_SIZE)
 
         # Count Au-NPS
-        masked_images = apply_masking_roi(raw_images)
-        segmented_images = apply_nanoparticles_segmentation(masked_images, PROCESSED_IMAGES_DIRECTORY)
+        segmented_images = apply_nanoparticles_segmentation(raw_images, filenames, SEGMENTED_IMAGES_DIRECTORY)
+        masked_images = apply_masking_roi(segmented_images, raw_images, filenames, MASKED_IMAGES_DIRECTORY)
         
+        # Count white pixels and store results in a DataFrame
+        counts = [(filename, count_white_pixels(img)) for img, filename in masked_images]
+        df_counts = pd.DataFrame(counts, columns=['Filename', 'White Pixel Count'])
+
+        # Save to Excel file
+        excel_file_path = os.path.join(RESULTS_DIRECTORY, 'white_pixel_counts.xlsx')
+        df_counts.to_excel(excel_file_path, index=False)
+        print(f"White pixel counts have been written to {excel_file_path}")
+
     except FileNotFoundError as fnf_err:
         print(f"Error: {fnf_err}")
     except ValueError as val_err:
